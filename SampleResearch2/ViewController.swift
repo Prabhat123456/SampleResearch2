9f596d44620b0da0551be70d8c56b86baa45b11b
Philips <prabhat.goel@philips.com>
2018-02-11 14:29:59 +0530

    Initial Commit

---
+++ SampleResearch2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleResearch2
+//
+//  Created by Philips on 11/02/18.
+//  Copyright © 2018 Philips. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
